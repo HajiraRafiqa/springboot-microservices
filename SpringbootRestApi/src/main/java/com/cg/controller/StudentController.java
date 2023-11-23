6597955ffc43a0096a0fe650de23a1e51472ca59
HajiraRafiqa <hajirarafiqa@gmail.com>
2023-11-23 22:38:40 +0530

    adding code

--- SpringbootRestApi/src/main/java/com/cg/controller/StudentController.java
+++ SpringbootRestApi/src/main/java/com/cg/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.bean.Student;
@@ -35,9 +36,17 @@ public class StudentController {
 	//{id} -> URI template variable
 	
 	@GetMapping("students/{id}")
-	public Student studentPathVariable(@PathVariable("id") int studentId)
+	public Student studentPathVariable(@PathVariable("id") int studentId,
+			@PathVariable("first-name") String firstName,
+            @PathVariable("last-name") String lastName)
 	{
 		return new Student(studentId, "B", "A");
 	}
 	
+	@GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id, firstName, lastName);
+    }
 }
